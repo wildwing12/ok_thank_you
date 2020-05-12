0104f1dbe1eabd46556454f4843c44288e4ef130
wildwing12 <46217342+wildwing12@users.noreply.github.com>
2020-05-12 17:48:03 +0900

    무저ㅣ

--- you/src/main/java/com/ok_thank/you/controller/HomeController.java
+++ you/src/main/java/com/ok_thank/you/controller/HomeController.java
@@ -1,16 +1,12 @@
 package com.ok_thank.you.controller;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.ok_thank.you.service.GsonService;
+import com.google.gson.Gson;
+import com.ok_thank.you.dto.Test2VO;
 
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
@@ -21,21 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 @Api(value="swag-rest-controller", description="HomeController")
 public class HomeController {
 	
-	@Autowired
-	private GsonService service;
-	
 	private String json;
 	
 	@GetMapping("/")
-	@CrossOrigin
-	public String Home() throws Exception {
-		json = service.getJson();
-		JsonElement element = new JsonParser().parse(json);
-		JsonObject obj = element.getAsJsonObject();
-		String idx = obj.get("idx").toString();
-		String name = obj.get("name").toString();
-		//System.out.println("idx: "+idx+", name: "+name);
-		if(StringUtils.isNotEmpty(idx) && StringUtils.isNotEmpty(name)) {
+	public String Home() {
+		Test2VO test = new Test2VO();
+		test.setIdx(3);
+		test.setName("JJUD");
+		Gson gson = new Gson();
+		json = gson.toJson(test).toString();
+		if(StringUtils.isNotEmpty(json)) {
 			log.info(json);
 			return json;
 		}else {
