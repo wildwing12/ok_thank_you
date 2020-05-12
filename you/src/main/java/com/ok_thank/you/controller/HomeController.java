fd58274ea1bcd33538979b34ccdf02946a6e6450
ggipk <ggipk@DESKTOP-979HGTJ>
2020-05-12 10:53:21 +0900

    Spring+mybatis+swagger 연동하셨닷

--- you/src/main/java/com/ok_thank/you/controller/HomeController.java
+++ you/src/main/java/com/ok_thank/you/controller/HomeController.java
@@ -1,16 +1,20 @@
 package com.ok_thank.you.controller;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 import com.ok_thank.you.dto.Test2VO;
 
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Controller
 @RestController
+@Api(value="swag-rest-controller", description="HomeController")
 public class HomeController {
 	
 	private String json;
---
+++ you/src/main/java/com/ok_thank/you/mapper/PdTableMapper.java
@@ -0,0 +1,16 @@
+package com.ok_thank.you.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface PdTableMapper {
+
+	List<Map<String, Object>> SelectList(Map<String, Object> map);
+
+
+}
