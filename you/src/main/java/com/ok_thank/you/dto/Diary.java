d3363c78df678dd10bd88223e90ab1a020bccbea
HYUNJOO <HYUNJOO@DESKTOP-BJ080PU>
2020-06-21 21:02:55 +0900

    Diary 테이블 VO

---
+++ you/src/main/java/com/ok_thank/you/dto/Diary.java
@@ -0,0 +1,18 @@
+package com.ok_thank.you.dto;
+
+import lombok.Data;
+
+@Data
+public class Diary {
+	
+	private Integer idx;
+	
+	private String writer;
+	
+	private String content;
+	
+	private String regDt;
+	
+	private String udtDt;
+
+}
