621e61e839334c7f84710a60ac39c3d5ea4db8e4
wildwing <ggipk1010@gmail.com>
2020-06-25 21:19:38 +0900

    Pager

--- you/src/main/java/com/ok_thank/you/dto/Pager.java
+++ you/src/main/java/com/ok_thank/you/dto/Pager.java
@@ -1,101 +1,50 @@
 package com.ok_thank.you.dto;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Data;
+
+@Data
 public class Pager {
 
-	// 현재페이지, 시작페이지, 끝페이지, 게시글 총 갯수, 페이지당 글 갯수, 마지막페이지, SQL쿼리에 쓸 start, end
-	private int nowPage, startPage, endPage, total, cntPerPage, lastPage, start, end;
-	private int cntPage = 5;
+	private Integer pageNum;
+	private Integer pageRowCnt;
+	private Integer pageDisplayCnt;
+	private Integer startRowNum;
+	private Integer totalRow;
+	private Integer totalPageCount;
+	private Integer startPageNum;
+	private Integer endPageNum;
+	private Integer endRowNum;
+	
+	public Map<String, Object> pageReturn(String pn, int tot , int display, int offset){
+		
+		this.pageNum = 0;
+		this.pageRowCnt = 10;
+		this.pageDisplayCnt = display;
+		this.endRowNum = offset;
+		
+		setPageNum(Integer.valueOf(pn)); 
+		
+		this.startRowNum = getPageNum() * getPageRowCnt();
+		this.totalRow = tot; //맵 밸류 2
+		this.totalPageCount = (int)Math.ceil(getTotalRow() / (double) getPageRowCnt());
+		this.startPageNum = 1+((getPageNum() / getPageDisplayCnt()) * getPageDisplayCnt());
+		this.endPageNum = getStartPageNum() + getPageDisplayCnt() -1;
 
-	public Pager() {
-	}
-	public Pager(int total, int nowPage, int cntPerPage) {
-		setNowPage(nowPage);
-		setCntPerPage(cntPerPage);
-		setTotal(total);
-		calcLastPage(getTotal(), getCntPerPage());
-		calcStartEndPage(getNowPage(), cntPage);
-		calcStartEnd(getNowPage(), getCntPerPage());
-	}
-	// 제일 마지막 페이지 계산
-	public void calcLastPage(int total, int cntPerPage) {
-		setLastPage((int) Math.ceil((double)total / (double)cntPerPage));
-	}
-	// 시작, 끝 페이지 계산
-	public void calcStartEndPage(int nowPage, int cntPage) {
-		setEndPage(((int)Math.ceil((double)nowPage / (double)cntPage)) * cntPage);
-		if (getLastPage() < getEndPage()) {
-			setEndPage(getLastPage());
+		if(getTotalPageCount() < getEndPageNum()){
+			setEndPageNum(getTotalPageCount());
 		}
-		setStartPage(getEndPage() - cntPage + 1);
-		if (getStartPage() < 1) {
-			setStartPage(1);
-		}
+		
+		Map<String, Object> page = new HashMap<String, Object>();
+		
+		page.put("startRowNum", getStartRowNum());
+		page.put("startPageNum", getStartPageNum());
+		page.put("endPageNum", getEndPageNum());
+		page.put("totalPageNum", getTotalPageCount());
+		page.put("endRowNum", getEndRowNum());
+		
+		return page;
 	}
-	// DB 쿼리에서 사용할 start, end값 계산
-	public void calcStartEnd(int nowPage, int cntPerPage) {
-		setEnd(nowPage * cntPerPage);
-		setStart(getEnd() - cntPerPage + 1);
-	}
-
-	public int getNowPage() {
-		return nowPage;
-	}
-	public void setNowPage(int nowPage) {
-		this.nowPage = nowPage;
-	}
-	public int getStartPage() {
-		return startPage;
-	}
-	public void setStartPage(int startPage) {
-		this.startPage = startPage;
-	}
-	public int getEndPage() {
-		return endPage;
-	}
-	public void setEndPage(int endPage) {
-		this.endPage = endPage;
-	}
-	public int getTotal() {
-		return total;
-	}
-	public void setTotal(int total) {
-		this.total = total;
-	}
-	public int getCntPerPage() {
-		return cntPerPage;
-	}
-	public void setCntPerPage(int cntPerPage) {
-		this.cntPerPage = cntPerPage;
-	}
-	public int getLastPage() {
-		return lastPage;
-	}
-	public void setLastPage(int lastPage) {
-		this.lastPage = lastPage;
-	}
-	public int getStart() {
-		return start;
-	}
-	public void setStart(int start) {
-		this.start = start;
-	}
-	public int getEnd() {
-		return end;
-	}
-	public void setEnd(int end) {
-		this.end = end;
-	}	
-	public int setCntPage() {
-		return cntPage;
-	}
-	public void getCntPage(int cntPage) {
-		this.cntPage = cntPage;
-	}
-	@Override
-	public String toString() {
-		return "PagingVO [nowPage=" + nowPage + ", startPage=" + startPage + ", endPage=" + endPage + ", total=" + total
-				+ ", cntPerPage=" + cntPerPage + ", lastPage=" + lastPage + ", start=" + start + ", end=" + end
-				+ ", cntPage=" + cntPage + "]";
-	}
-
 }
