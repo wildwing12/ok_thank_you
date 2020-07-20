28381c9f01cca465541a8dc457e807f6e9d4289e
wildwing12 <46217342+wildwing12@users.noreply.github.com>
2020-07-20 11:02:33 +0900

    파일 다운로드 공통 유틸화 성공

--- you/src/main/java/com/ok_thank/you/service/Impl/DiaryServiceImpl.java
+++ you/src/main/java/com/ok_thank/you/service/Impl/DiaryServiceImpl.java
@@ -31,6 +31,7 @@ import com.ok_thank.you.dto.DiarySearchReq;
 import com.ok_thank.you.dto.TestFileTEst;
 import com.ok_thank.you.mapper.DiaryMapper;
 import com.ok_thank.you.service.DiaryService;
+import com.ok_thank.you.util.FileUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -283,46 +284,7 @@ public class DiaryServiceImpl implements DiaryService{
 	@Override
 	public void fileDownload(HttpServletRequest request, HttpServletResponse response, String filename, String oriname,
 			String realFilename) {
-		try {// 파일 이름이 깨지면 사용하세요.
-			String browser = request.getHeader("User-Agent");//파일 인코딩
-			if(browser.contains("MSIE")|| browser.contains("Trident")||browser.contains("Chrome")) {
-				filename = URLEncoder.encode(filename,"UTF-8").replaceAll("\\+","%20");
-				oriname =  URLEncoder.encode(oriname,"UTF-8").replaceAll("\\+","%20");
-			} else {
-				filename = new String(filename.getBytes("UTF-8"),"ISO-8859-1");
-				oriname = new String(oriname.getBytes("UTF-8"),"ISO-8859-1");
-			}
-		} catch (UnsupportedEncodingException ex) {
-			log.error("에러 발생 :UnsupportedEncodingException => {}", ex.getMessage());
-		}
-		realFilename = "C:\\temp\\"+filename;
-		log.info(">>>>>>>>>>>>>>>>>>>>>>"+realFilename);
-		log.info("리일 파일 네임=>{}", realFilename);
-		File file1 = new  File(realFilename);
-		if(!file1.exists()) {
-			return ;
-		}
-
-		// 파일명 지정        
-		response.setContentType("application/octer-stream");
-		//response.setHeader("Content-Transfer-Encoding", "binary;");
-		response.setHeader("Content-Disposition", "attachment; filename=" + oriname );
-		try {
-			OutputStream os = response.getOutputStream();
-			FileInputStream fis = new FileInputStream(realFilename);
-
-			int ncount = 0;
-			byte[] bytes = new byte[1024*10];
-
-			while ((ncount = fis.read(bytes)) != -1 ) {
-				os.write(bytes, 0, ncount);
-			}
-			fis.close();
-			os.close();
-		} catch (Exception e) {
-			System.out.println("FileNotFoundException : " + e);
-			log.info("에러내용 => {}", e.getMessage());
-		}
+		new FileUtils(request, response, filename, oriname, realFilename);
 		
 	}
 
